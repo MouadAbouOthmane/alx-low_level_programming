64853b0adaffbf3c170d35414f54a5aca8508f08
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-03-17 23:02:07 +0100

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - portal
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		printf("%d", c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
