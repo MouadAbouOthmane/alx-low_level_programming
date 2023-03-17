4d3fb58ec143e6666da477d0b38f219ffdc682ef
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-03-17 01:10:03 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,16 +5,16 @@
  *
  * Return: Always 0 (Success)
  *
- * Filedes: file des 
- * Buf: is the buffer 
- * Nbyte: number of byte 
+ * Filedes: file des
+ * Buf: is the buffer
+ * Nbyte: number of byte
  */
 
 /* prototype of function write*/
-int write(int filedes, const char *buf, unsigned int nbyte); 
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 	/* main fuction */
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
