3149054f62bf3488f1e6fddc3a146c8ebbe9e3ae
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-03-17 23:16:38 +0100

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - portal
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
