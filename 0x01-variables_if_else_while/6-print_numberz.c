395ede9f9b206fe4629255b246be96239ad24400
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-03-17 23:13:56 +0100

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - portal
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + 48);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
