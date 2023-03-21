13e810f2704ff52da460b14c4d40ba28c98f9961
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-03-21 16:25:51 +0000

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
+/**
+ * print_alphabet - function that prints the alphabet in lowercase
+ */
+
 void print_alphabet(void)
 {
 	char c;
-	for ( c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
