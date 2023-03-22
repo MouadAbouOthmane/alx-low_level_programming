e14fdbb3c3e70c46fd89460a6cc89867a5f250c8
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-03-22 23:06:25 +0000

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - point portal 
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, n, sum;
+
+	n = 1024;
+	sum = 0;
+
+	for (i = 1; i < n; i++)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
