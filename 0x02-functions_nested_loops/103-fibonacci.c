206946a546cac1cdc7415707b7a14dae7ece468d
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-03-22 23:50:22 +0000

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - point portal
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	long int n1, n2, r, sum;
+
+	n1 = 1;
+	n2 = 0;
+	r = 0;
+	sum = 0;
+
+	for (i = 0; i < 35; i++)
+	{
+		r = n1 + n2;
+		n2 = n1;
+		n1 = r;
+
+		if (r % 2 == 0)
+		{
+			sum += r;
+		}
+	}
+	printf("%ld\n", sum);
+
+
+	return (0);
+}
