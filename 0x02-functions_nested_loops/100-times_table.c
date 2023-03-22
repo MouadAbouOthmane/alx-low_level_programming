332502b4091a56e84984f2354e76c83005ff79a2
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-03-22 22:42:51 +0000

    dwadwa

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,10 +14,10 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			_putchar(',');
 
 			for (j = 1; j <= n; j++)
 			{
+				_putchar(',');
 				r = j * i;
 				_putchar(' ');
 				if ((r / 100) > 0)
@@ -37,10 +37,6 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + r);
-				}
-				if (j != n)
-				{
-					_putchar(',');
 				}
 			}
 			_putchar('\n');
