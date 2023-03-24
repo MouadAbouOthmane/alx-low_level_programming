cc4cefb7a759917e0e8bcdc479a62a27f1715908
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-03-24 01:56:20 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 /**
  * main - point portal
  *
- * Return: always 0 
+ * Return: always 0
  */
 
 int main(void)
@@ -27,9 +27,8 @@ int main(void)
 		else
 		{
 			printf("%d ", i);
-	
 		}
 	}
-	
+
 	printf("\n");
 }
