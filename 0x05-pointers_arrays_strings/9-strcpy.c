2605eb773e8e9b3551bd12449e4d76b0db82e5e1
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-03-28 00:44:51 +0000

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy -  function that copies the string pointed
+ * @dest: destination
+ * @src: source
+ *
+ * Return: value the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
