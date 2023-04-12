1e1b3a4273d7a5a7e1d6435f6bdc11f2ceb2012e
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-04-12 01:32:57 +0000

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 
-/** free_grid - function that frees a 2 dimensional grid previously created.
- *
+/** 
+ * free_grid - function that frees a 2 dimensional grid previously created.
  * @grid: 2 dimensional grid.
  * @h: height.
  */
