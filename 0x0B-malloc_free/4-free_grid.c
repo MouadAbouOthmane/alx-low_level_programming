7ab6daad579c09212fab0bb98f0a9fc3f4a7009b
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-04-12 01:34:18 +0000

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 
-/** 
+/**
  * free_grid - function that frees a 2 dimensional grid previously created.
  * @grid: 2 dimensional grid.
  * @h: height.
@@ -9,9 +9,13 @@
 
 void free_grid(int **grid, int h)
 {
-	while (--h)
+	int i;
+
+	i = 0
+	while (i < h)
 	{
-		free(grid[h]);
+		free(grid[i);
+		i++;
 	}
 	free(grid);
 }
