1c2e31c8a5b36ba1db0dd220eb94a280c388d997
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-04-13 14:38:40 +0000

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size2 = (int) n;
 	}
 
-	s = malloc(sizeof(char) * (size1 + size2 + 1));
+	s = malloc(sizeof(char) * (size1 + n + 1));
 
 	if (s == 0)
 	{
