7d2be12209c47da49d18e46a6918722555858592
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-04-18 22:28:19 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,14 +28,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	d->name = malloc(sizeof(char) * (strlen(name) + 1));
-	if (!d->name)
+	if (!(*d).name)
 	{
 		free(d);
 		return (0);
 	}
 
 	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if (!d->owner)
+	if (!(*d).owner)
 	{
 		free(d);
 		free(d->name);
