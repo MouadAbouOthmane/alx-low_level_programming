29b812181867cc026d605ff18279f61e89876a90
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-04-19 23:07:50 +0000

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && size && action)
 	{
-		while (array <= lst)
+		while (array < lst)
 		{
 			action(*array++);
 		}
