300106af3cebab7d42fed84841d7fd36cc8569c4
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-04-27 23:11:17 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d} %s\n", strlen(h->str), h->str ? h->str : "(nil)");
+		printf("[%ld] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
 		i++;
 	}
