a138c7afc37ed0205e365faf172825ff19d896f2
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-05-03 00:18:28 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint -  adds a new node at the beginning of a listint_t list.
  *
- * @h: head of node.
+ * @head: head of node.
  * @n: integer.
  *
  * Return: the address of element
