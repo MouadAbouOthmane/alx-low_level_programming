ba86d82a943de1f2f5bb2c9091ea991d91bdb29e
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-05-09 00:30:10 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,24 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len;
+	int file, new_file, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
+		for (; text_content[len]; len++)
 			;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	new_file = write(file, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (file == -1 || new_file == -1)
 		return (-1);
 
-	close(fd);
+	close(file);
 
 	return (1);
 }
