fff60b9db577fec3a4e96ad99228008411697c19
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-05-09 00:28:16 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @filename: name of the file.
  * @text_content: to a string to write to the file.
  *
- * Return: If the function fails - -1.
+ * Return: if the function fails -1.
  */
 
 int create_file(const char *filename, char *text_content)
@@ -18,8 +18,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len = 0; text_content[len]; len++)
+			;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
