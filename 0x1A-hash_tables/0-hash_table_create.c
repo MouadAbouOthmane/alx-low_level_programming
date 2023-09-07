13edeaf8c581dc57001d7dff6814e744b62f0faa
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-07 18:28:30 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -25,7 +25,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 	for (i = 0; i < table->size; i++)
-    	table->array[i] = NULL;
+	table->array[i] = NULL;
 
 	return (table);
 }
