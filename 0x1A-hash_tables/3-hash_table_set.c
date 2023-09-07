8991638a43a26959fcb85bfe4d88033f232800ce
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-07 18:28:10 +0000

    3. >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ *
+ * @ht: hash table.
+ * @key: key string.
+ * @value: string value.
+ *
+ * Return: 1 if succeeded Otherwise 0.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+	unsigned long int index;
+	hash_node_t *head;
+	hash_node_t *new_node;
+
+	if (!ht || !key)
+		return (0);
+
+	index = key_index(key, ht->size);
+
+	head = ht->array[index]; 
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = malloc(strlen(key) + 1);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = malloc(strlen(value) + 1);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
+	if (head)
+		new_node->next = head;
+	ht->array[index] = new_node;
+	return (1);
+}
