25ab6ea1047ac46205ece0df7d9959960a9fd8e3
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-07 19:54:22 +0000

    4. >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ *
+ * @ht: hash table.
+ * @key: key string.
+ *
+ * Return: the value associated with the element, or
+ * NULL if key couldn’t be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+	unsigned long int index;
+	hash_node_t *head;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size);
+
+	head = ht->array[index];
+
+	while (head)
+    {
+        if (strcmp(head->key, key) == 0)
+        {
+            return (head->value);
+        }
+        head = head->next;
+    }
+
+    return(NULL);    
+}
