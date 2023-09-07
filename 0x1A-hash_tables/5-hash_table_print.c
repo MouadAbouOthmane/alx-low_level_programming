0261031dbd573ac8e458abf1b02c9a0a1159144c
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-07 22:40:24 +0100

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,30 +5,30 @@
  *
  * @ht: hash table.
  *
- */
+*/
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
+	unsigned long int i;
 	hash_node_t *head;
-    int is_first = 0;
+	int is_first = 0;
 
 	if (!ht)
 		return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        head = ht->array[i];
-
-        while (head)
-        {
-            if (is_first == 1)
-                printf(", ");
-            printf("'%s': '%s'", head->key, head->value);
-            head = head->next;
-            is_first = 1;
-        }
-    }
-    printf("}\n");
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		head = ht->array[i];
+	
+		while (head)
+		{
+			if (is_first == 1)
+				printf(", ");
+			printf("'%s': '%s'", head->key, head->value);
+			head = head->next;
+			is_first = 1;
+		}
+	}
+	printf("}\n");
 
 }
