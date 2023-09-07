bd49ce1c24412b8e1f70b864d871f0e1360e0551
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-07 21:30:43 +0000

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ *
+ * @ht: hash table.
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+	hash_node_t *head;
+    int is_first = 0;
+
+	if (!ht)
+		return;
+
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        head = ht->array[i];
+        printf("\n");
+
+        while (head)
+        {
+            if (is_first == 1)
+                printf(", ");
+            printf("'%s': '%s'", head->key, head->value);
+            head = head->next;
+            is_first = 1;
+        }
+    }
+    printf("}\n");
+
+}
