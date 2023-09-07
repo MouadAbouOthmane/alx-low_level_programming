200ab8fac4ba99e3aad9d48e4de4b2932d6dccca
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-07 19:29:28 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index(key, ht->size);
 
-	head = ht->array[index]; 
+	head = ht->array[index];
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
